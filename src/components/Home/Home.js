0ea55f850f564cf5face25e043aaafdba6a3f225
tduncan14 <nbatre1@yahoo.com>
2022-06-13 20:51:56 -0700

    keep moving forward

---
+++ src/components/Home/Home.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Home.css'
+
+
+
+
+
+
+
+
+const  Home = () => {
+
+
+
+
+
+
+
+return (
+     <>
+      <div className="home">
+
+
+      <div className="homeright">
+
+
+
+        <h1 className="topH">SO, YOU WANT TO TRAVEL TO</h1>
+
+          <h1 className="space1"> SPACE</h1>
+
+        <p className="spaceP">Let's face it; if you want to go to space, you might as well <br /> genuinely go to outer space and not
+          hover kind of on the <br /> edge of it. Well sit back, and relax because we'll give you a  <br /> truly out of this world
+          experience!
+        </p>
+
+      </div>
+
+      <div className="homeleft">
+ 
+          <span className="circle"> Explore</span>
+        
+      </div>
+
+      </div>
+     </>
+    )
+
+
+}
+
+
+
+
+export default Home
